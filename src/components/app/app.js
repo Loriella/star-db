b7b45e5c783eabfd435fa40191c438b0cde7d07d
Loriella <loris.orlova@gmail.com>
2019-05-21 15:01:44 +0300

    created component people-page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 import './app.css';
-
+import PeoplePage from '../people-page';
 export default class App extends Component {
 
   state = {
-    showRandomPlanet: true,
-    selectedPerson: null
+    showRandomPlanet: true
   };
 
   toggleRandomPlanet = () => {
@@ -37,20 +34,16 @@ export default class App extends Component {
         <Header />
         { planet }
 
-        <button
-          className="toggle-planet btn btn-warning btn-lg"
-          onClick={this.toggleRandomPlanet}>
-          Toggle Random Planet
-        </button>
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected}/>
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
         </div>
+
+        <PeoplePage />
+
       </div>
     );
   }
