ec39e9d2ea9321e85d2d906e92923ea71c78b107
Loriella <loris.orlova@gmail.com>
2019-05-30 17:47:57 +0300

    creates a component pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,21 +5,12 @@ import ErrorBoundry from '../error-boundry';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 import { SwapiServiceProvider } from '../swapi-service-context';
-import Row from '../row';
-import {
-  PersonDetails,
-  PlanetDetails,
-  StarshipDetails,
-  PersonList,
-  PlanetList,
-  StarshipList
-} from '../sw-components';
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 import './app.css';
 
 export default class App extends Component {
 
   state = {
-    showRandomPlanet: true,
     swapiService: new SwapiService()
   };
 
@@ -37,30 +28,16 @@ export default class App extends Component {
 
   render() {
 
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
-
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
           <div className="stardb-app">
             <Header onServiceChange={this.onServiceChange} />
 
-            { planet }
-
-            <Row
-              left={<PersonList />}
-              right={<PersonDetails itemId={11} />}
-            />
-            <Row
-              left={<PlanetList />}
-              right={<PlanetDetails itemId={5} />}
-            />
-            <Row
-              left={<StarshipList />}
-              right={<StarshipDetails itemId={9} />}
-            />
+            <RandomPlanet />
+            <PeoplePage />
+            <PlanetsPage />
+            <StarshipsPage />
 
           </div>
         </SwapiServiceProvider>
