001603f49de48005b240442020d8c00ceacf167b
Loriella <loris.orlova@gmail.com>
2019-06-05 16:27:56 +0300

    creates a component login-page

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,10 +2,12 @@ import PeoplePage from './people-page';
 import PlanetsPage from './planets-page';
 import StarshipsPage from './starships-page';
 import SecretPage from './secret-page';
+import LoginPage from './login-page';
 
 export {
   PeoplePage,
   PlanetsPage,
   StarshipsPage,
-  SecretPage
+  SecretPage,
+  LoginPage
 };
